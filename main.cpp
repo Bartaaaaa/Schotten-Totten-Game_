74e8d5ca654e90a8ebe6459a14d4b47a2f986a27
NathanP <nathan.piteux@etu.utc.fr>
2023-04-19 14:41:16 +0200

    test

--- main.cpp
+++ main.cpp
@@ -3,7 +3,10 @@
 //
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << endl;
+    cout << "Hello, World2!" << endl;
     return 0;
 }
