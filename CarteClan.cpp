cc49fd039d0246b3e17610733f7bd942444c8e93
Samohtlep <thomas.lepretre@iclloud.com>
2023-04-26 15:11:06 +0200

    test commit + ostream CarteClan

--- CarteClan.cpp
+++ CarteClan.cpp
@@ -4,3 +4,7 @@
 
 #include "CarteClan.h"
 
+ostream& operator<<(ostream& os, const CarteClan& carteClan) {
+    os << "CarteClan :\n\tPuissance : " << carteClan.getPuissance() << "\n\tCouleur : " << carteClan.getCouleur();
+    return os;
+}
