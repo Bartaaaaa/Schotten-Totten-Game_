0c497c726dabc1c3aa0c9a6385a91852366062be
MattScal <matteoscalabre@gmail.com>
2023-05-13 12:29:50 +0200

    ajout des methodes piocher

--- Pioche.cpp
+++ Pioche.cpp
@@ -37,15 +37,74 @@
         }
     }
 
-    const Carte& Pioche::piocher()
-    {
-        if(estVide()) throw CarteException("La pioche est vide");
+    const Carte& Pioche::piocher() {
+        if(estVide()){
+            throw CarteException("La pioche est vide.");
+        }
         m_nb--;
-        return *m_cartes[getNbCartes()];
+        const Carte*carte = m_cartes[getNbCartes()];
+        if (dynamic_cast<const CarteTactique*>(carte))
+        {
+            const CarteTactique* carteTactique = dynamic_cast<const CarteTactique*>(carte);
+            return *new CarteTactique(*carteTactique);
+        }
+        else if (dynamic_cast<const CarteClan*>(carte))
+        {
+            const CarteClan* carteClan = dynamic_cast<const CarteClan*>(carte);
+            return *new CarteClan(*carteClan);
+        }
 
+        throw CarteException("L'objet pioché n'est pas une carte");
+    }
+
+    const CarteClan& Pioche::piocherCarteClan() {
+        if (estVide()) {
+            throw CarteException("La pioche est vide");
+        }
+        m_nb--;
+        const Carte*carte = m_cartes[getNbCartes()];
+
+        const CarteClan* carteclan = dynamic_cast<const CarteClan*>(carte);
+        if (!carteclan) {
+            throw CarteException("La carte piochée n'est pas une carte clan.");
+        }
+        return *carteclan;
+    }
+
+    const CarteTactique& Pioche::piocherCarteTactique(){
+        if(estVide()){
+            throw CarteException("La pioche est vide.");
+        }
+        m_nb--;
+        const Carte*carte=m_cartes[getNbCartes()];
+        const CarteTactique* cartetactique = dynamic_cast<const CarteTactique*>(carte);
+        if(!cartetactique){
+            throw CarteException("La carte piochée n'est pas une carte tactique");
+        }
+        return *cartetactique;
     }
 
     Pioche::~Pioche()
     {
         delete [] m_cartes;
     }
+
+    /* utilisation des pioches
+    JeuClan& jc = JeuClan::getJeuClan();
+    JeuTactique& jt = JeuTactique::getJeuTactique();
+    Pioche pc(jc);
+    Pioche pt(jt);
+    cout <<"taille pioche clan : "<<pc.getNbCartes()<<"\n";
+    cout <<"taille pioche tactique : "<<pt.getNbCartes()<<"\n";
+    const CarteClan& cc=dynamic_cast<const CarteClan&>(pc.piocher());
+    const CarteTactique& ct=dynamic_cast<const CarteTactique&>(pt.piocher());
+    cout <<"puissance carte clan : "<<cc.getPuissance()<<"\n";
+    cout <<"couleur carte clan : "<<cc.getCouleur()<<"\n";
+    cout<<"carte tactique : "<<ct.getNom()<<"\n";
+    auto& cc2=pc.piocherCarteClan();
+    auto& ct2=pt.piocherCarteTactique();
+    cout <<"puissance carte clan 2 : "<<cc2.getPuissance()<<"\n";
+    cout <<"couleur carte clan 2 : "<<cc2.getCouleur()<<"\n";
+    cout<<"carte tactique 2 : "<<ct2.getNom()<<"\n";
+    cout <<"taille pioche clan : "<<pc.getNbCartes()<<"\n";
+    cout <<"taille pioche tactique : "<<pt.getNbCartes()<<"\n";*/
