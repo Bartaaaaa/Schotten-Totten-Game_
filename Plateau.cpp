9eb8a4f2bf15c79084192360a62917998b4277df
NathanP <nathan.piteux@etu.utc.fr>
2023-05-27 09:49:14 +0200

    update methode poser

--- Plateau.cpp
+++ Plateau.cpp
@@ -10,14 +10,12 @@ void Plateau::poser(Borne borne, CarteClan* carte){
 
         int fcombi = borne.m_cartesj1->getForceCombi();
         //std::cout << "Force combi : " << fcombi << std::endl;
-        borne.m_cartesj1->setTotalPuissance(borne.m_cartesj1->getForceCombi() + static_cast<int>(carte->getPuissance()));
         borne.m_cartesj1->ajouterCarte(carte);
         // TODO : ajouter une fonction retirer carte avec cette cate dans la main du joueur
     }
     else{
         int fcombi = borne.m_cartesj2->getForceCombi();
         //std::cout << "Force combi : " << fcombi << std::endl;
-        borne.m_cartesj2->setTotalPuissance(borne.m_cartesj1->getForceCombi() + static_cast<int>(carte->getPuissance()));
         borne.m_cartesj2->ajouterCarte(carte);
         // TODO : ajouter une fonction retirer carte avec cette cate dans la main du joueur
     }
