024b057af5879425883bbb517f2f1b54b27eb214
Bart <bartlomiejgrzadziel2@gmail.com>
2023-06-15 08:57:30 +0200

    DEBUGAGE ENORME : PATCHNOTE 1.0

--- Controleur.cpp
+++ Controleur.cpp
@@ -1575,8 +1575,6 @@ void Controleur::fin_de_partie(){
 
 
     // Libérer la mémoire des objets membres des différentes classes
-    delete[] m_plateau->m_joueur1->getMain();
-    delete[] m_plateau->m_joueur2->getMain();
 
     // Vider et supprimer les conteneurs de pointeurs
     vector<Carte*> cartesMain1 = m_plateau->m_joueur1->getMain()->getCartes();
@@ -1585,11 +1583,14 @@ void Controleur::fin_de_partie(){
     }
     cartesMain1.clear();
 
+
     vector<Carte*> cartesMain2 = m_plateau->m_joueur2->getMain()->getCartes();
     for (int i = 0; i < cartesMain2.size(); i++) {
         delete cartesMain2[i];
     }
     cartesMain2.clear();
+    delete[] m_plateau->m_joueur1->getMain();
+    delete[] m_plateau->m_joueur2->getMain();
     for (int i=0;i<9;i++){
         delete m_plateau->m_bornes[i];
     }
